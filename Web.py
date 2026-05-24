8b667d9cbdf1277155fa017d2d59ce28b6efb613
Nuomi-web <lsszxyycwy@163.com>
2026-05-24 11:41:54 +0800

    Update Web.py

--- Web.py
+++ Web.py
@@ -264,7 +264,7 @@ if st.sidebar.button("Predict"):
             input_df.iloc[0, :], 
             feature_names=feature_label, 
             matplotlib=True, 
-            contribution_threshold=0.2
+            contribution_threshold=0.15
         )
         plt.savefig("shap_force_plot.png", bbox_inches='tight', dpi=120)
         plt.close()
