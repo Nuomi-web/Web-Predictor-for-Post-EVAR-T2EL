82524f32aea5e0154a0e8bed156a8e6a8b6473c3
Nuomi-web <lsszxyycwy@163.com>
2026-07-05 13:09:49 +0800

    Update Web.py

--- Web.py
+++ Web.py
@@ -64,7 +64,6 @@ default_values = {
     "AAA_square_glcm_Imc1": 10.20636933,
     "IMA patency": "Yes",
     "Maximum aneurysm diameter (mm)": 48.06
-    "Maximum aneurysm diameter (mm)": ""
 }
 
 # ===============================
